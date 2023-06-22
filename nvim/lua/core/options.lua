1d51fa53c99d6f8f664c476ff4e904915898a450
Hong Quang <quang.th@hodfords.com>
2023-06-22 10:10:25 +0700

    feat: update nvim

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,12 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
