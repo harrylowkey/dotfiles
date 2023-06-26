7a4f04e789e5ae666c2ead04f9454ca8c5b13bae
harrylowkey <harrydang.tech@gmail.com>
2023-06-26 23:05:03 +0700

    feat: add remove all text for telescope searching

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -41,7 +41,6 @@ keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window max
 -- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 
-
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
