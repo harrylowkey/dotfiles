11d7d2b60b8793eb2bf867eeaccbc6cccdf8750b
harrylowkey <harrydang.tech@gmail.com>
2023-07-06 10:21:02 +0700

    feat: config resize pane & window in tmux

--- nvim/lua/mappings/toggle-term.lua
+++ nvim/lua/mappings/toggle-term.lua
@@ -1,3 +1,4 @@
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>3", "<cmd>ToggleTerm<CR>")
+keymap.set("n", "<leader>3", "<cmd>ToggleTerm direction=horizontal<CR>")
+keymap.set("n", "<leader>4", "<cmd>ToggleTerm direction=vertical size=40<CR>")
