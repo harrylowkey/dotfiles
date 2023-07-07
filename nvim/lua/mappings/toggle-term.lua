ef0a098c0e55b476571bb4b6a6f465e82e923798
harrylowkey <harrydang.tech@gmail.com>
2023-07-07 14:54:16 +0700

    chore: update nvim toggleterm keymap config

--- nvim/lua/mappings/toggle-term.lua
+++ nvim/lua/mappings/toggle-term.lua
@@ -1,5 +1,4 @@
 local keymap = vim.keymap
-local opts = { buffer = 0 }
 
 keymap.set("n", "<leader>3", "<cmd>ToggleTerm direction=horizontal<CR>")
 keymap.set("n", "<leader>4", "<cmd>ToggleTerm direction=vertical size=40<CR>")
@@ -7,7 +6,6 @@ keymap.set("n", "<leader>4", "<cmd>ToggleTerm direction=vertical size=40<CR>")
 function _G.set_terminal_keymaps()
    local opts = { buffer = 0 }
    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
