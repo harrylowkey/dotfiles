fa533b1ecc1732c06937fb07be8f96fd8b759019
harrylowkey <harrydang.tech@gmail.com>
2023-07-18 11:44:05 +0700

    feat: add sketchy-bar plugin

---
+++ sketchybar/colors.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+export BLACK=#181926
+export WHITE=#ffffff
+export RED=#ff0000
+export GREEN=#60ff60
+export BLUE=#0000ff
+export YELLOW=#ffff00
+export ORANGE=#ffa500
+export MAGENTA=#ff80ff
+export GRAY=#808080
+
+export NORD4=#D8DEE9
+export NORD10=#147b7b
+
+# General bar colors
+export BAR_COLOR=0xff1e1e2e
+export BACKGROUND_1=0xff3c3e4f
+export BACKGROUND_2=0xff494d64
+
+export POPUP_BACKGROUND_COLOR=$BLACK
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
