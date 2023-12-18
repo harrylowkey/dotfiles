9eecd00683f886c6102dbf06c206faa48c18364d
harrylowkey <harydang.tech@gmail.com>
2023-12-18 13:43:58 +0700

    fix colorizer

--- nvim/lua/config/plugins/colorizer.lua
+++ nvim/lua/config/plugins/colorizer.lua
@@ -1,16 +1,20 @@
--- FIXME: Not working
 require("colorizer").setup({
-	filetypes = {
-		"html",
-		"css",
-		"javascript",
-		"typescript",
-		"typescriptreact",
-		"javascriptreact",
-		"lua",
-	},
-	user_default_options = {
-		mode = "background",
-		tailwind = false, -- Enable tailwind colors
-	},
+  filetypes = {
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "typescriptreact",
+    "javascriptreact",
+    "lua",
+  },
+  user_default_options = {
+    mode = "foreground",
+    tailwind = false, -- Enable tailwind colors
+  },
 })
+
+-- :ColorizerAttachToBuffer
+-- :ColorizerDetachFromBuffer
+-- :ColorizerReloadAllBuffers
+-- :ColorizerToggle
