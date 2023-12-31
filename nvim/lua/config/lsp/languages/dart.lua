c8873283a979b6a5c19965ccda80bbf159049fb8
harrylowkey <harrydang.tech@gmail.com>
2024-01-01 02:24:20 +0700

    config alacritty terminal

--- nvim/lua/config/lsp/languages/dart.lua
+++ nvim/lua/config/lsp/languages/dart.lua
@@ -3,8 +3,8 @@ local capabilities = require("config.lsp.utils/capabilities")
 local on_attach = require("config.lsp.utils/on_attach")
 local handlers = require("config.lsp.utils/handlers")
 
-lspconfig.dart.setup({
-  capabilities = capabilities,
-  handlers = handlers,
-  on_attach = on_attach,
-})
+-- lspconfig.dart.setup({
+--   capabilities = capabilities,
+--   handlers = handlers,
+--   on_attach = on_attach,
+-- })
--- nvim/lua/config/lsp/languages/init.lua
+++ nvim/lua/config/lsp/languages/init.lua
@@ -1,6 +1,5 @@
 require("config.lsp.languages.bash")
 require("config.lsp.languages.css")
-require("config.lsp.languages.dart")
 require("config.lsp.languages.eslint")
 require("config.lsp.languages.html")
 require("config.lsp.languages.json")
