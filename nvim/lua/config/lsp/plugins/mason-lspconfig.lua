d934aef8fe330013a2d90e2b267e603570c850b3
harrylowkey <harrydang.tech@gmail.com>
2024-03-02 20:44:32 +0700

    q

--- nvim/lua/config/lsp/plugins/mason-lspconfig.lua
+++ nvim/lua/config/lsp/plugins/mason-lspconfig.lua
@@ -1,17 +1,18 @@
 local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
-  ensure_installed = {
-    "html",
-    "cssls",
-    "eslint",
-    "tailwindcss",
-    "lua_ls",
-    "pyright",
-    "terraformls",
-    "jsonls",
-    "bashls",
-    "yamlls",
-  },
-  automatic_installation = true, -- not related to ensure_installed
+	ensure_installed = {
+		"html",
+		"cssls",
+		"eslint",
+		"tailwindcss",
+		"lua_ls",
+		"pyright",
+		"terraformls",
+		"jsonls",
+		"bashls",
+		"yamlls",
+		"gopls",
+	},
+	automatic_installation = true, -- not related to ensure_installed
 })
