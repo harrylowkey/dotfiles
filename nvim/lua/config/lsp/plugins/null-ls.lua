bcfec7131b3ecb941d81bd8673f0ec94ad5d230b
harrylowkey <harrydang.tech@gmail.com>
2024-03-13 14:40:26 +0700

    remote cspell plugin

--- nvim/lua/config/lsp/plugins/null-ls.lua
+++ nvim/lua/config/lsp/plugins/null-ls.lua
@@ -1,34 +1,31 @@
 local null_ls = require("null-ls")
 local on_attach = require("config.lsp.utils.on_attach")
 
-local formatting = null_ls.builtins.formatting     -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics   -- to setup linters
+local formatting = null_ls.builtins.formatting -- to setup formatters
+local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 local code_actions = null_ls.builtins.code_actions -- to setup linters
 
 null_ls.setup({
-  sources = {
-    diagnostics.cspell,
-    code_actions.cspell,
+    sources = {
+        -- lua
+        formatting.stylua,
 
-    -- lua
-    formatting.stylua,
+        -- python
+        diagnostics.ruff,
+        formatting.ruff,
+        formatting.isort,
 
-    -- python
-    diagnostics.ruff,
-    formatting.ruff,
-    formatting.isort,
+        -- js/ts
+        diagnostics.eslint_d.with({
+            condition = function(utils)
+                return utils.root_has_file(".eslintrc.js")
+            end,
+        }),
+        code_actions.eslint_d,
+        -- formatting.prettier,
 
-    -- js/ts
-    diagnostics.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file(".eslintrc.js")
-      end,
-    }),
-    code_actions.eslint_d,
-    formatting.prettier,
-
-    --sh
-    formatting.beautysh,
-  },
-  on_attach = on_attach,
+        --sh
+        formatting.beautysh,
+    },
+    on_attach = on_attach,
 })
