7700e6db894068f27be20415f54c1a1494926566
harrylowkey <harrydang.tech@gmail.com>
2024-03-14 11:09:58 +0700

    change nvim theme

--- sketchybar/controller/wifi.sh
+++ sketchybar/controller/wifi.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
-CURR_IP="$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')"
-CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
-SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //' | awk '{for(i=1; i<=NF; i++) printf "%s",substr($i,1,1)}')"
-
 WIFIACTIVEICON=􀙇
 WIFIINACTIVEICON=􀙈
 
-if [ "$CURR_TX" = "" ]; then
-	sketchybar --set $NAME icon=$WIFIINACTIVEICON label="disconnected |"
+CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //' | awk '{for(i=1; i<=NF; i++) printf "%s",substr($i,1,1)}')"
+CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
+
+if [ "$SSID" = "" ]; then
+  sketchybar --set $NAME label="Disconnected |" icon=$WIFIINACTIVEICON
 else
-    SPEED=$(awk -v num="$CURR_TX" 'BEGIN{ printf "%.2f MBps", num/8 }')
-	sketchybar --set $NAME icon=$WIFIACTIVEICON label="$SSID $SPEED |"
+  SPEED=$(awk -v num="$CURR_TX" 'BEGIN{ printf "%.2f MBps", num/8 }')
+
+  sketchybar --set $NAME label="$SSID $SPEED |" icon=$WIFIACTIVEICON
 fi
