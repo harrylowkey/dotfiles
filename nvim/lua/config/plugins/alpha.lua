6c5b16b9609c906f065baedece087fa2210a01fb
harrylowkey <harrydang.tech@gmail.com>
2024-03-14 14:13:49 +0700

    install more themes

--- nvim/lua/config/plugins/alpha.lua
+++ nvim/lua/config/plugins/alpha.lua
@@ -15,54 +15,56 @@ local logo = [[
 ]]
 
 local function getGreeting(name)
-	local tableTime = os.date("*t")
-	local datetime = os.date(" %Y-%m-%d   %H:%M:%S")
-	local hour = tableTime.hour
-	local greetingsTable = {
-		[1] = "  Sleep well",
-		[2] = "  Good morning",
-		[3] = "  Good afternoon",
-		[4] = "  Good evening",
-		[5] = "󰖔  Good night",
-	}
-	local greetingIndex = 0
-	if hour == 23 or hour < 7 then
-		greetingIndex = 1
-	elseif hour < 12 then
-		greetingIndex = 2
-	elseif hour >= 12 and hour < 18 then
-		greetingIndex = 3
-	elseif hour >= 18 and hour < 21 then
-		greetingIndex = 4
-	elseif hour >= 21 then
-		greetingIndex = 5
-	end
-	return "\t" .. greetingsTable[greetingIndex] .. ", " .. name .. "\t" .. datetime
+    local tableTime = os.date("*t")
+    local datetime = os.date(" %Y-%m-%d   %H:%M:%S")
+    local hour = tableTime.hour
+    local greetingsTable = {
+        [1] = "  Sleep well",
+        [2] = "  Good morning",
+        [3] = "  Good afternoon",
+        [4] = "  Good evening",
+        [5] = "󰖔  Good night",
+    }
+    local greetingIndex = 0
+    if hour == 23 or hour < 7 then
+        greetingIndex = 1
+    elseif hour < 12 then
+        greetingIndex = 2
+    elseif hour >= 12 and hour < 18 then
+        greetingIndex = 3
+    elseif hour >= 18 and hour < 21 then
+        greetingIndex = 4
+    elseif hour >= 21 then
+        greetingIndex = 5
+    end
+    return "\t" .. greetingsTable[greetingIndex] .. ", " .. name .. "\t" .. datetime
 end
 
 local userName = "harry"
 local greeting = getGreeting(userName)
 local header = {
-	type = "text",
-	val = vim.split(logo .. "\n" .. greeting, "\n"),
-	opts = {
-		position = "center",
-		hl = "Type",
-		-- wrap = "overflow",
-	},
+    type = "text",
+    val = vim.split(logo .. "\n" .. greeting, "\n"),
+    opts = {
+        position = "center",
+        hl = "Type",
+        -- wrap = "overflow",
+    },
 }
 local buttons = {
-	type = "group",
-	val = {
-		{ type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
-		{ type = "padding", val = 1 },
-		dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
-		dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
-		dashboard.button("s", "  Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-		dashboard.button("u", "󰂖  Update plugins", ":PackerSync<CR>"),
-		dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
-	},
-	position = "center",
+    type = "group",
+    val = {
+        { type = "text", val = "Quick links", opts = { hl = "SpecialComment", position = "center" } },
+        { type = "padding", val = 1 },
+        dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+        dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+        dashboard.button("m", "󰂖  Mason", ":Mason<CR>"),
+        dashboard.button("z", "󰂖  Lazy", ":Lazy<CR>"),
+        dashboard.button("u", "󰂖  Update plugins", ":Lazy sync<CR>"),
+        dashboard.button("i", "󰂖  Update lsp", ":MasonUpdate<CR>"),
+        dashboard.button("q", "  Quit NVIM", ":qa<CR>"),
+    },
+    position = "center",
 }
 
 config.layout[2] = header
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,123 +1,122 @@
 return {
-    -- theme
-    -- {
-    --   "navarasu/onedark.nvim",
-    --   config = function()
-    --     require("onedark").setup({
-    --       style = "deep",
-    --     })
-    --     require("onedark").load()
-    --   end,
-    --   opts = {},
-    -- },
-    {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("config.plugins.theme")
-        end,
+  -- theme
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        style = "deep",
+      })
+    end,
+    opts = {},
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("config.plugins.kanagawa_theme")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("config.plugins.catppuccin_theme")
+    end,
+  },
+  ---
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("config.plugins.transparent")
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("config.plugins.nvim-web-devicons")
+    end,
+  },
+  { "onsails/lspkind.nvim" },
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      require("config.plugins.nvim-notify")
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+    config = function()
+      require("config.plugins.noice")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("config.plugins.lualine")
+    end,
+  },
+  -- animation
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+      opts.cursor = {
+        enable = false,
+      }
+    end,
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
     },
-    -- {
-    --   "catppuccin/nvim",
-    --   name = "catppuccin",
-    --   priority = 1000,
-    --   config = function()
-    --     require("config.plugins.theme")
-    --   end,
-    -- },
-    --
-    {
-        "xiyaowong/transparent.nvim",
-        config = function()
-            require("config.plugins.transparent")
-        end,
-    },
-    {
-        "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("config.plugins.nvim-web-devicons")
-        end,
-    },
-    { "onsails/lspkind.nvim" },
-    {
-        "rcarriga/nvim-notify",
-        config = function()
-            require("config.plugins.nvim-notify")
-        end,
-    },
-    {
-        "folke/noice.nvim",
-        dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-        config = function()
-            require("config.plugins.noice")
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("config.plugins.lualine")
-        end,
-    },
-    -- animation
-    {
-        "echasnovski/mini.animate",
-        event = "VeryLazy",
-        opts = function(_, opts)
-            opts.scroll = {
-                enable = false,
-            }
-            opts.cursor = {
-                enable = false,
-            }
-        end,
-    },
-    {
-        "folke/zen-mode.nvim",
-        cmd = "ZenMode",
-        opts = {
-            plugins = {
-                gitsigns = true,
-                tmux = true,
-                kitty = { enabled = false, font = "+2" },
-            },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+    config = function()
+      require("config.plugins.indent-blankline")
+    end,
+  },
+  {
+    "echasnovski/mini.indentscope",
+    version = false,     -- wait till new 0.7.0 release to put it back on semver
+    event = "VeryLazy",
+    opts = {},
+    config = function()
+      require("config.plugins.indent-scope")
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
         },
-        keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-        config = function()
-            require("config.plugins.indent-blankline")
+        callback = function()
+          vim.b.miniindentscope_disable = true
         end,
-    },
-    {
-        "echasnovski/mini.indentscope",
-        version = false, -- wait till new 0.7.0 release to put it back on semver
-        event = "VeryLazy",
-        opts = {},
-        config = function()
-            require("config.plugins.indent-scope")
-        end,
-        init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-                callback = function()
-                    vim.b.miniindentscope_disable = true
-                end,
-            })
-        end,
-    },
+      })
+    end,
+  },
 }
