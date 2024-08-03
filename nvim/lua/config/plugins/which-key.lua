ccb21b1223cef779deb0604b9720cf996d2cf509
harrylowkey <harrydang.tech@gmail.com>
2024-08-03 16:36:34 +0700

    feat: migrate deprepcated config of which-key

--- nvim/lua/config/plugins/which-key.lua
+++ nvim/lua/config/plugins/which-key.lua
@@ -22,26 +22,10 @@ wk.setup({
             g = false, -- bindings for prefixed with g
         },
     },
-    -- add operators that will trigger motion and text object completion
-    -- to enable all native operators, set the preset / operators plugin above
-    operators = { gc = "Comments" },
-    key_labels = {
-        -- override the label used to display some keys. It doesn't effect WK in any other way.
-        -- For example:
-        -- ["<space>"] = "SPC",
-        -- ["<cr>"] = "RET",
-        -- ["<tab>"] = "TAB",
-    },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
         group = "+", -- symbol prepended to a group
-    },
-    window = {
-        border = NeoVim.ui.float.border or "rounded", -- none, single, double, shadow, rounded
-        position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     },
     layout = {
         height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -49,18 +33,8 @@ wk.setup({
         spacing = 4, -- spacing between columns
         align = "left", -- align columns left, center or right
     },
-    ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
     show_help = true, -- show help message on the command line when the popup is visible
-    -- triggers = "auto", -- automatically setup triggers
     triggers = { "<leader>" }, -- or specify a list manually
-    triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for key maps that start with a native binding
-        -- most people should not need to change this
-        i = { "j", "k" },
-        v = { "j", "k" },
-    },
 })
 
 local opts = {
@@ -72,6 +46,203 @@ local opts = {
     nowait = false, -- use `nowait` when creating keymaps
 }
 
+wk.add({
+    { "<leader>/", group = "Nvim", nowait = false, remap = false },
+    { "<leader>//", "<cmd>Alpha<CR>", desc = "open dashboard", nowait = false, remap = false },
+    { "<leader>/d", "<cmd>Lazy<CR>", desc = "Lazy dashboard", nowait = false, remap = false },
+    { "<leader>/s", "<cmd>Lazy sync<CR>", desc = "Lazy update plugins", nowait = false, remap = false },
+    { "<leader>1", hidden = true, nowait = false, remap = false },
+    { "<leader>2", hidden = true, nowait = false, remap = false },
+    { "<leader>3", hidden = true, nowait = false, remap = false },
+    { "<leader>4", hidden = true, nowait = false, remap = false },
+    { "<leader>5", hidden = true, nowait = false, remap = false },
+    { "<leader>6", hidden = true, nowait = false, remap = false },
+    { "<leader>7", hidden = true, nowait = false, remap = false },
+    { "<leader>8", hidden = true, nowait = false, remap = false },
+    { "<leader>9", hidden = true, nowait = false, remap = false },
+    { "<leader>?", "<cmd>Telescope help_tags<CR>", desc = "Telescope helps", nowait = false, remap = false },
+    { "<leader>c", group = "colorscheme theme", nowait = false, remap = false },
+    { "<leader>ct", "<cmd>Telescope colorscheme<CR>", desc = "color schemes", nowait = false, remap = false },
+    { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Tree toggle", nowait = false, remap = false },
+    { "<leader>f", group = "Telescope", nowait = false, remap = false },
+    {
+        "<leader>f/",
+        "<cmd>Telescope current_buffer_fuzzy_find<cr>",
+        desc = "search text in current file",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "list open buffers", nowait = false, remap = false },
+    {
+        "<leader>fc",
+        "<cmd>Telescope grep_string<cr>",
+        desc = "search text under cursor",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "search files", nowait = false, remap = false },
+    {
+        "<leader>fh",
+        "<cmd>Telescope find_files hidden=true<cr>",
+        desc = "search hidden files",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "telescope key maps config", nowait = false, remap = false },
+    { "<leader>fs", "<cmd>Telescope live_grep<cr>", desc = "search text", nowait = false, remap = false },
+    {
+        "<leader>ft",
+        "<cmd>lua vim.cmd('NvimTreeFocus');vim.cmd('Telescope find_files hidden=true')<cr>",
+        desc = "search files in tree",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>g", group = "Git", nowait = false, remap = false },
+    {
+        "<leader>gA",
+        "<cmd>!git add .<CR>",
+        desc = "add all",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>gB",
+        "<cmd>Telescope git_branches<CR>",
+        desc = "branches",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>ga",
+        "<cmd>!git add %:p<cr>",
+        desc = "add current",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>gb",
+        "<cmd>Gitsigns blame<CR>",
+        desc = "blame",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>gd", group = "Git diff", nowait = false, remap = false },
+    {
+        "<leader>gdc",
+        ":DiffviewClose<CR>",
+        desc = "Close git diff",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>gdf",
+        ":DiffviewFileHistory %<CR>",
+        desc = "Git status current file",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>gdh",
+        ":DiffviewFileHistory<CR>",
+        desc = "Git status all files history",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>gdo",
+        ":DiffviewOpen<CR>",
+        desc = "Git status",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>gl", group = "Log", nowait = false, remap = false },
+    {
+        "<leader>gla",
+        '<cmd>lua require("config.plugins.telescope").my_git_commits()<CR>',
+        desc = "commits (Telescope)",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>glc",
+        '<cmd>lua require("config.plugins.telescope").my_git_bcommits()<CR>',
+        desc = "buffer commits (Telescope)",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>gm", desc = "blame line", nowait = false, remap = false },
+    { "<leader>k", group = "K9s", nowait = false, remap = false },
+    { "<leader>k9", "<cmd>lua _k9s_toggle()<CR>", desc = "k9s toggle", nowait = false, remap = false },
+    { "<leader>l", group = "Lazy Git & LSP Saga", nowait = false, remap = false },
+    {
+        "<leader>lc",
+        "<cmd>Lspsaga show_cursor_diagnostics<CR>",
+        desc = "diagnostics under cursor",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", desc = "lazy git toggle", nowait = false, remap = false },
+    {
+        "<leader>ll",
+        "<cmd>Lspsaga show_line_diagnostics<CR>",
+        desc = "diagnostics current line",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>m", group = "Markdown & Harpoon", nowait = false, remap = false },
+    { "<leader>md", "<cmd>MarkdownPreviewToggle<CR>", desc = "markdown preview toggle", nowait = false, remap = false },
+    {
+        "<leader>mf",
+        ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
+        desc = "harpoon quick list marked files",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>mm", ":Telescope harpoon marks<CR>", desc = "harpoon list marked files", nowait = false, remap = false },
+    {
+        "<leader>mn",
+        ":lua require('harpoon.ui').nav_next()<CR>",
+        desc = "harpoon open next marked file",
+        nowait = false,
+        remap = false,
+    },
+    {
+        "<leader>mp",
+        ":lua require('harpoon.ui').nav_prev()<CR>",
+        desc = "harpoon open previous marked file",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>r", group = "Rename text", nowait = false, remap = false },
+    { "<leader>rn", "lua vim.lsp.buf.rename()<CR>", desc = "rename", nowait = false, remap = false },
+    { "<leader>s", group = "Search", nowait = false, remap = false },
+    {
+        "<leader>sc",
+        '<cmd>lua require("config.plugins.telescope").command_history()<CR>',
+        desc = "command history",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>sf", "<cmd>Telescope oldfiles hidden=true<CR>", desc = "file history", nowait = false, remap = false },
+    { "<leader>t", group = "Todo & Trouble", nowait = false, remap = false },
+    {
+        "<leader>tc",
+        ":TroubleToggle document_diagnostics<CR>",
+        desc = "trouble diagnostics current file",
+        nowait = false,
+        remap = false,
+    },
+    { "<leader>td", ":TodoTelescope<CR>", desc = "todo toggle", nowait = false, remap = false },
+    { "<leader>tt", ":TroubleToggle<CR>", desc = "trouble toggle", nowait = false, remap = false },
+    {
+        "<leader>tw",
+        ":TroubleToggle workspace_diagnostics<CR>",
+        desc = "trouble diagnostics working project",
+        nowait = false,
+        remap = false,
+    },
+}, opts)
+
 local visual_opts = {
     mode = "v", -- NORMAL mode
     prefix = "<leader>",
@@ -81,166 +252,22 @@ local visual_opts = {
     nowait = false, -- use `nowait` when creating keymaps
 }
 
-local normal_mode_mappings = {
-    -- ignore
-    ["1"] = "which_key_ignore",
-    ["2"] = "which_key_ignore",
-    ["3"] = "which_key_ignore",
-    ["4"] = "which_key_ignore",
-    ["5"] = "which_key_ignore",
-    ["6"] = "which_key_ignore",
-    ["7"] = "which_key_ignore",
-    ["8"] = "which_key_ignore",
-    ["9"] = "which_key_ignore",
-
-    -- single
-    ["?"] = { "<cmd>Telescope help_tags<CR>", "Telescope helps" },
-    ["e"] = { "<cmd>NvimTreeToggle<CR>", "Tree toggle" },
-
-    ["/"] = {
-        name = "Nvim",
-        ["/"] = { "<cmd>Alpha<CR>", "open dashboard" },
-        d = { "<cmd>Lazy<CR>", "Lazy dashboard" },
-        s = { "<cmd>Lazy sync<CR>", "Lazy update plugins" },
+wk.add({
+    {
+        mode = { "v" },
+        { "<leader>a", group = "Actions", nowait = false, remap = false },
+        { "<leader>ac", desc = "comment box", nowait = false, remap = false },
+        { "<leader>c", group = "LSP", nowait = false, remap = false },
+        { "<leader>ca", desc = "range code action", nowait = false, remap = false },
+        { "<leader>cf", desc = "range format", nowait = false, remap = false },
+        { "<leader>g", group = "Git", nowait = false, remap = false },
+        { "<leader>gh", group = "Hunk", nowait = false, remap = false },
+        { "<leader>ghr", desc = "reset hunk", nowait = false, remap = false },
+        { "<leader>ghs", desc = "stage hunk", nowait = false, remap = false },
+        { "<leader>p", group = "Project", nowait = false, remap = false },
+        { "<leader>pr", desc = "refactor", nowait = false, remap = false },
+        { "<leader>s", "<cmd>'<,'>sort<CR>", desc = "sort", nowait = false, remap = false },
+        { "<leader>t", group = "Table Mode", nowait = false, remap = false },
+        { "<leader>tt", desc = "tableize", nowait = false, remap = false },
     },
-
-    c = {
-        name = "colorscheme theme",
-        t = { "<cmd>Telescope colorscheme<CR>", "color schemes" },
-    },
-
-    --  ╭──────────────────────────────────────────────────────────╮
-    --  │ 	d = {                                                 │
-    --  │ 		name = "debug",                                   │
-    --  │ 		a = { "attach" },                                 │
-    --  │ 		b = { "breakpoint" },                             │
-    --  │ 		c = { "continue" },                               │
-    --  │ 		c = { "close ui" },                               │
-    --  │ 		d = { "continue" },                               │
-    --  │ 		h = { "visual hover" },                           │
-    --  │ 		i = { "step into" },                              │
-    --  │ 		o = { "step over" },                              │
-    --  │ 		o = { "step out" },                               │
-    --  │ 		r = { "repl" },                                   │
-    --  │ 		s = { "scopes" },                                 │
-    --  │ 		t = { "terminate" },                              │
-    --  │ 		v = { "log variable" },                           │
-    --  │ 		v = { "log variable above" },                     │
-    --  │ 		w = { "watches" },                                │
-    --  │ 	},                                                    │
-    --  ╰──────────────────────────────────────────────────────────╯
-
-    g = {
-        name = "Git",
-        a = { "<cmd>!git add %:p<cr>", "add current" },
-        A = { "<cmd>!git add .<CR>", "add all" },
-        b = { "<cmd>Gitsigns blame<CR>", "blame" },
-        B = { "<cmd>Telescope git_branches<CR>", "branches" },
-        d = {
-            name = "Git diff",
-            o = { ":DiffviewOpen<CR>", "Git status" },
-            c = { ":DiffviewClose<CR>", "Close git diff" },
-            f = { ":DiffviewFileHistory %<CR>", "Git status current file" },
-            h = { ":DiffviewFileHistory<CR>", "Git status all files history" },
-        },
-        l = {
-            name = "Log",
-            a = { '<cmd>lua require("config.plugins.telescope").my_git_commits()<CR>', "commits (Telescope)" },
-            c = { '<cmd>lua require("config.plugins.telescope").my_git_bcommits()<CR>', "buffer commits (Telescope)" },
-        },
-        m = { "blame line" },
-    },
-
-    s = {
-        name = "Search",
-        f = { "<cmd>Telescope oldfiles hidden=true<CR>", "file history" },
-        c = { '<cmd>lua require("config.plugins.telescope").command_history()<CR>', "command history" },
-    },
-
-    f = {
-        name = "Telescope",
-        f = { "<cmd>Telescope find_files<cr>", "search files" },
-        s = { "<cmd>Telescope live_grep<cr>", "search text" },
-        c = { "<cmd>Telescope grep_string<cr>", "search text under cursor" },
-        b = { "<cmd>Telescope buffers<cr>", "list open buffers" },
-        h = { "<cmd>Telescope find_files hidden=true<cr>", "search hidden files" },
-        t = {
-            "<cmd>lua vim.cmd('NvimTreeFocus');vim.cmd('Telescope find_files hidden=true')<cr>",
-            "search files in tree",
-        },
-        k = { "<cmd>Telescope keymaps<cr>", "telescope key maps config" },
-        ["/"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "search text in current file" },
-    },
-
-    k = {
-        name = "K9s",
-        ["9"] = { "<cmd>lua _k9s_toggle()<CR>", "k9s toggle" },
-    },
-
-    l = {
-        name = "Lazy Git & LSP Saga",
-        g = { "<cmd>lua _lazygit_toggle()<CR>", "lazy git toggle" },
-        c = { "<cmd>Lspsaga show_cursor_diagnostics<CR>", "diagnostics under cursor" },
-        l = { "<cmd>Lspsaga show_line_diagnostics<CR>", "diagnostics current line" },
-    },
-
-    m = {
-        name = "Markdown & Harpoon",
-        d = { "<cmd>MarkdownPreviewToggle<CR>", "markdown preview toggle" },
-        m = { ":Telescope harpoon marks<CR>", "harpoon list marked files" },
-        f = { ":lua require('harpoon.ui').toggle_quick_menu()<CR>", "harpoon quick list marked files" },
-        n = { ":lua require('harpoon.ui').nav_next()<CR>", "harpoon open next marked file" },
-        p = { ":lua require('harpoon.ui').nav_prev()<CR>", "harpoon open previous marked file" },
-    },
-
-    t = {
-        name = "Todo & Trouble",
-        d = { ":TodoTelescope<CR>", "todo toggle" },
-        t = { ":TroubleToggle<CR>", "trouble toggle" },
-        w = { ":TroubleToggle workspace_diagnostics<CR>", "trouble diagnostics working project" },
-        c = { ":TroubleToggle document_diagnostics<CR>", "trouble diagnostics current file" },
-    },
-
-    r = {
-        name = "Rename text",
-        n = { "lua vim.lsp.buf.rename()<CR>", "rename" },
-    },
-}
-
-local visual_mode_mappings = {
-    -- single
-    ["s"] = { "<cmd>'<,'>sort<CR>", "sort" },
-
-    a = {
-        name = "Actions",
-        c = { "comment box" },
-    },
-
-    c = {
-        name = "LSP",
-        a = { "range code action" },
-        f = { "range format" },
-    },
-
-    g = {
-        name = "Git",
-        h = {
-            name = "Hunk",
-            r = "reset hunk",
-            s = "stage hunk",
-        },
-    },
-
-    p = {
-        name = "Project",
-        r = { "refactor" },
-    },
-
-    t = {
-        name = "Table Mode",
-        t = { "tableize" },
-    },
-}
-
-wk.register(normal_mode_mappings, opts)
-wk.register(visual_mode_mappings, visual_opts)
+}, visual_opts)
--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,24 +1,25 @@
 return {
-	{
-		"goolord/alpha-nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("config.plugins.alpha")
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		config = function()
-			require("config.plugins.which-key")
-		end,
-	},
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+    {
+        "goolord/alpha-nvim",
+        event = "VimEnter",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("config.plugins.alpha")
+        end,
+    },
+    {
+        "folke/which-key.nvim",
+        config = function()
+            require("config.plugins.which-key")
+        end,
+    },
+    { "echasnovski/mini.nvim", version = false },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
