b09c2df5c140c6b53df6126b788055d2a524da11
harrylowkey <harrydang.tech@gmail.com>
2024-08-26 22:18:41 +0700

    fix: spectre add -E file after replacing

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -41,7 +41,18 @@ return {
         "nvim-pack/nvim-spectre",
         dependencies = "nvim-lua/plenary.nvim",
         config = function()
-            require("spectre").setup()
+            require("spectre").setup({
+                replace_engine = {
+                    ["sed"] = {
+                        cmd = "sed",
+                        args = {
+                            "-i",
+                            "",
+                            "-E",
+                        },
+                    },
+                },
+            })
         end,
     },
     {
