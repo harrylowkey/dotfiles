b06a392224c495291e7468846f3bf2372bc635a3
harrylowkey <harrydang.tech@gmail.com>
2024-09-09 21:24:40 +0700

    feat: add render-markdown

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -91,11 +91,21 @@ return {
         config = true,
     },
     {
-        "rest-nvim/rest.nvim",
-        ft = "http",
-        dependencies = { "luarocks.nvim" },
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {},
+        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
         config = function()
-            require("config.plugins.rest")
+            require("render-markdown").setup()
         end,
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     },
+    -- {
+    --     "rest-nvim/rest.nvim",
+    --     ft = "http",
+    --     dependencies = { "luarocks.nvim" },
+    --     config = function()
+    --         require("config.plugins.rest")
+    --     end,
+    -- },
 }
