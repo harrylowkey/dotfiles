1f9e23c336f2f0987344b8a347be3c2231012d01
harrylowkey <harrydang.tech@gmail.com>
2025-05-14 15:36:57 +0700

    feat: enable rest

---
+++ nvim/lua/mappings/rest.lua
@@ -0,0 +1,4 @@
+-- Lua
+vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>rl", "<cmd>Rest logs<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>re", "<cmd>Rest env select<cr>", { silent = true, noremap = true })
--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -121,4 +121,17 @@ return {
             },
         },
     },
+    {
+        "rest-nvim/rest.nvim",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            opts = function(_, opts)
+                opts.ensure_installed = opts.ensure_installed or {}
+                table.insert(opts.ensure_installed, "http")
+            end,
+            config = function()
+                require("config.plugins.rest")
+            end,
+        },
+    },
 }
