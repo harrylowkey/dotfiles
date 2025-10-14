1ca834dbc304e6f7d9ad924bc84f4a61849274e9
harrylowkey <harrydang.tech@gmail.com>
2025-10-15 03:28:43 +0700

    feat: remove unsued libs

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,31 +1,39 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    cmd = "Codeium",
-    build = ":Codeium Auth",
+    {
+        "Exafunction/codeium.nvim",
+        cmd = "Codeium",
+        build = ":Codeium Auth",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+        },
+        config = function()
+            require("codeium").setup({})
+        end,
+    },
+    {
+        "Exafunction/codeium.vim",
+        event = "BufEnter",
+    },
+
+    "greggh/claude-code.nvim",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
+        "nvim-lua/plenary.nvim", -- Required for git operations
     },
     config = function()
-      require("codeium").setup({})
+        require("config.claudecode")
     end,
-  },
-  {
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-  },
-  -- {
-  --   "olimorris/codecompanion.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "echasnovski/mini.diff",
-  --     "stevearc/dressing.nvim",
-  --     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },       -- Optional: For prettier markdown rendering
-  --   },
-  --   config = function()
-  --     require("config.plugins.codecompanion")
-  --   end,
-  -- },
+    -- {
+    --   "olimorris/codecompanion.nvim",
+    --   dependencies = {
+    --     "nvim-lua/plenary.nvim",
+    --     "nvim-treesitter/nvim-treesitter",
+    --     "echasnovski/mini.diff",
+    --     "stevearc/dressing.nvim",
+    --     { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } },       -- Optional: For prettier markdown rendering
+    --   },
+    --   config = function()
+    --     require("config.plugins.codecompanion")
+    --   end,
+    -- },
 }
