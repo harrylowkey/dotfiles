e035c71d4f9650ef986fd226d26c57192b57e26f
harrylowkey <harrydang.tech@gmail.com>
2026-06-25 06:41:36 +0700

    sketchybar: docker item - nerd-font icon + bash controller
    view: 🐳 emoji -> nerd-font docker glyph (󰡨).
    controller: rewrite fish -> bash (if/fi), set icon.color with label.color.

--- sketchybar/view/docker.sh
+++ sketchybar/view/docker.sh
@@ -2,9 +2,7 @@
 
 sketchybar -m --add item docker left                         \
               --set docker update_freq=5                     \
-                    icon="🐳"                                \
-                    icon.y_offset=2                          \
-                    label.y_offset=-1                        \
+                    icon=󰡨                                   \
                     icon.font="$LABEL:Bold:14"               \
                     cpu script="$PLUGIN_DIR/docker.sh"       \
                     click_script="$PLUGIN_DIR/toggle_docker.sh" \
